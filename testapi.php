57139d2f5556cea24d79328e1f02f9411cb98b16
dominicattal <attalientidominic@gmail.com>
2026-02-26 17:32:56 -0500

    testapi for real

---
+++ testapi.php
@@ -0,0 +1,31 @@
+#!/bin/php
+<?php
+
+$ini = parse_ini_file(".api.ini", false);
+$key = $ini["API_KEY"];
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://api.themoviedb.org/3/authentication",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => [
+    "Authorization: Bearer $key",
+    "accept: application/json"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
