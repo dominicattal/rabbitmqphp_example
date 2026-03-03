6315d433d46b94c0bd9fd0b0b59230355f853ac5
Alyson M <alm57@njit.edu>
2026-03-03 12:36:13 -0500

    add changes

--- sample/higherlower.php
+++ sample/higherlower.php
@@ -2,8 +2,14 @@
 <html>
 <body>
 <h1>Higher or Lower?</h1>
+<h2>Guess which movie or tv show is rated the highest!"</h2>
+<button type="button" onclick="game()">Play Now!</button>
+<script>
+function game(){
+ 
+}
+</script>
 <?php
-echo "Guess which movie or tv show is rated the highest!";
 ?>
 </body>
 </html>
