aa5f44bbcddc17fc0e643051988cb658b733a777
Alyson M <alm57@njit.edu>
2026-03-06 14:50:46 -0500

    replace emojis with actaully movies, and work on logic

--- sample/higherlower.php
+++ sample/higherlower.php
@@ -2,28 +2,20 @@
 <html>
 <body>
 <style>
-.gameArea {
+.gameArea{
 	display:grid;
 	grid-template-columns:25% 25%;
 }
-.sad1{
-width:100px;
-height:100px;
-}
-.sad2{
-width:100px;
-height:100px;
+.movieArea{
+	width: 100px;
+	height:100px;
 }
 </style>
 <h1>Higher or Lower?</h1>
 <p>Guess which movie or tv show is rated the highest!</p>
 <div class="gameArea" id="gameArea">
-	<div id="movie1" style="background-color:blue;">
-		<img src="https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Sad-Face-Emoji.png" class="sad2">
-	</div>
-	<div id="movie2" style="background-color:red;">
-	<img id="sad1" src="https://www.cambridge.org/elt/blog/wp-content/uploads/2019/07/Sad-Face-Emoji.png" class="sad1">
-	</div>
+	<div id="movie1" class="movieArea" style="background-color:blue;"></div>
+	<div id="movie2" class="movieArea" style="background-color:red;"></div>
 </div>
 <button id="higher1">Higher</button>
 <button id="lower1">Lower</button>
@@ -43,16 +35,30 @@ height:100px;
 <button id="lower3">Lower</button>
 <button id="next3">Next</button>
 <script>
+const movies=[
+	{title:"IT", rating: 8.1},
+	{title:"Jaws", rating: 10},
+	{title:"Chips", rating: 3.1},
+	{title:"Rings", rating: 1.1},
+	{title:"Mouse", rating:7},
+	{title:"Door", rating:2}
+];
+document.getElementById("movie1").innerHTML=movies[0].title;
+document.getElementById("movie2").innerHTML=movies[1].title;
+document.getElementById("movie3").innerHTML=movies[2].title;
+document.getElementById("movie4").innerHTML=movies[3].title;
+document.getElementById("movie5").innerHTML=movies[4].title;
+document.getElementById("movie6").innerHTML=movies[5].title;
 const btnH=document.getElementById("higher1");
 btnH.addEventListener("click", 
 function(){ 
-	document.getElementById("movie1").innerHTML="hello";
+	document.getElementById("movie1").innerHTML=movies[0].rating;
 }
 );
 const btnL=document.getElementById("lower1");
 btnL.addEventListener("click",
 function(){
-	document.getElementById("movie2").innerHTML="loser";
+	document.getElementById("movie2").innerHTML=movies[1].rating;
 }
 );
 const btnN=document.getElementById("next1");
@@ -63,12 +69,13 @@ function(){
 );
 function verdict(){
 	//If element higher was clicked, and api of movie 1 > rating than movie 2,say correct, if false, incorrect
-	
+	if(movies[0].rating>movies[1].rating){
+		document.getElementById("gameArea").innerHTML="Correct";
+	}	
+	else{
+		document.getElementById("gameArea").innerHTML="Incorrect";
+	}
 }
-=======
-	document.getElementById("gameArea").innerHTML="new movies";
-}
-);
 
 </script>
 <?php
