0b0a9e3acabfc82e58cea7ab01c41ca06dc13dac
Alyson M <alm57@njit.edu>
2026-03-06 21:17:54 -0500

    Game is working, logic is corrected. Style sheet was added. API needed, and possibly store score

--- sample/higherlower.php
+++ sample/higherlower.php
@@ -1,35 +1,27 @@
 <!DOCTYPE html>
 <html>
-<body>
-<style>
-.gameArea{
-	display:grid;
-	grid-template-columns:20% 20%;
-}
-.movieArea{
-	width:150px;
-	height:150px;
-}
-</style>
-<h1>Higher or Lower?</h1>
+<link rel="stylesheet" href="madd.css">
+<body class="higherlower-body">
+<h1 style="font-size:40px; color:#FF5E5B;">Higher or Lower?</h1>
 <p>Guess which movie or tv show is rated the highest!</p>
+<p style="font-size:10px; padding:10px;">Getting a question correct earns points, but incorrect will deduct them</p>
 <div id="result"></div>
-<div class="gameArea" id="gameArea">
-	<div id="movie1" class="movieArea" style="background-color:blue;"></div>
-	<div id="movie2" class="movieArea" style="background-color:red;"></div>
+<div class="gameArea">
+	<div id="movie1" class="movieAreaA"></div>
+	<div id="movie2" class="movieAreaB"></div>
 </div>
 	<div id="result1"></div>
 	<button id="higher1">Higher</button>
 	<button id="lower1">Lower</button>
 <div class="gameArea" id="gameArea">
-	<div id="movie3" class="movieArea" style="background-color:blue;"></div>
-	<div id="movie4" class="movieArea" style="background-color:red;"></div>
+	<div id="movie3" class="movieAreaA"></div>
+	<div id="movie4" class="movieAreaB"></div>
 </div>
 	<button id="higher2">Higher</button>
 	<button id="lower2">Lower</button>
 <div class="gameArea" id="gameArea">
-	<div id="movie5" class="movieArea" style="background-color:blue;"></div>
-	<div id="movie6" class="movieArea" style="background-color:red;"></div>
+	<div id="movie5" class="movieAreaA"></div>
+	<div id="movie6" class="movieAreaB"></div>
 </div>
 	<button id="higher3">Higher</button>
 	<button id="lower3">Lower</button>
@@ -61,40 +53,52 @@ btnH1.addEventListener("click", function(){
 	verdict(movies[0].rating,movies[1].rating);
 	document.getElementById("movie1").innerHTML=movies[0].rating;
 	document.getElementById("movie2").innerHTML=movies[1].rating;
+	btnH1.disabled=true;
+	btnL1.disabled=true;
 });
 btnL1.addEventListener("click",function(){
-	verdict(movies[0].rating,movies[1].rating);
+	verdict(movies[1].rating,movies[0].rating);
 	document.getElementById("movie1").innerHTML=movies[0].rating;
 	document.getElementById("movie2").innerHTML=movies[1].rating;
+	btnH1.disabled=true;
+	btnL1.disabled=true;
 });
 btnH2.addEventListener("click",function(){
 	verdict(movies[2].rating,movies[3].rating);
 	document.getElementById("movie3").innerHTML=movies[2].rating;
 	document.getElementById("movie4").innerHTML=movies[3].rating;
+	btnH2.disabled=true;
+	btnL2.disabled=true;
 });
 btnL2.addEventListener("click",function(){
-	verdict(movies[2].rating,movies[3].rating);
+	verdict(movies[3].rating,movies[2].rating);
 	document.getElementById("movie3").innerHTML=movies[2].rating;
 	document.getElementById("movie4").innerHTML=movies[3].rating;
+	btnH2.disabled=true;
+	btnL2.disabled=true;
 });
 btnH3.addEventListener("click",function(){
 	verdict(movies[4].rating,movies[5].rating);
 	document.getElementById("movie5").innerHTML=movies[4].rating;
 	document.getElementById("movie6").innerHTML=movies[5].rating;
+	btnH3.disabled=true;
+	btnL3.disabled=true;
 });
 btnL3.addEventListener("click",function(){
-	verdict(movies[4].rating,movies[5].rating);
+	verdict(movies[5].rating,movies[4].rating);
 	document.getElementById("movie5").innerHTML=movies[4].rating;
 	document.getElementById("movie6").innerHTML=movies[5].rating;
+	btnH3.disabled=true;
+	btnL3.disabled=true;
 });
 function verdict(rating1, rating2){
 	if(rating1>rating2){
-		document.getElementById("result").innerHTML="Correct";
+		alert("Correct");
 		score++;
 		document.getElementById("score").innerHTML=score;
 	}	
 	else{
-		document.getElementById("result").innerHTML="Incorrect";
+		alert("Incorrect!");
 		score--;
 		document.getElementById("score").innerHTML=score;
 	}
