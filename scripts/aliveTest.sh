dc0d71f0e601ddb0c96865644ef84b3c348fa8a9
Matthew Einbinder <einbindermattwork@gmail.com>
2026-04-07 15:52:48 -0400

    Post Git corruption for 9 or so time

--- scripts/aliveTest.sh
+++ scripts/aliveTest.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-i=0
-
 if ping -c 1 100.111.93.122 > /dev/null 2>&1; then
   echo "online!"
 else
--- scripts/pingDB1.sh
+++ scripts/pingDB1.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-i=0
-
 if ping -c 1 127.0.0.1 > /dev/null 2>&1; then
   echo "online!"
 else
