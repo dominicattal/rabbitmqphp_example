d517a8b46d37fd5f5f9aceaf8d9ef8d7cd74f733
ddb33 <ddb33@njit.edu>
2026-04-08 00:03:25 -0400

    added systemd files, apache configuration file, and apache installation in apt_web.sh

--- deploy/apt_web.sh
+++ deploy/apt_web.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-apt install php rabbitmq-server php-amqp apache2
+apt install php rabbitmq-server php-amqp apache2 libapache2-mod-php php-mysql php-curl curl
+
+project_dir=$(pwd)
+ln -sf "$project_dir" /var/www/madd
+chmod +x /home/$USER
+chmod -R 755 "$project_dir"
+cp "$project_dir/config/apache.conf" /etc/apache2/sites-available/apache.conf
+a2ensite apache.conf
+systemctl reload apache2
