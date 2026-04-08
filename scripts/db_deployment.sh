bb119cb810210f9bed29ce7e637fd778c478a4d9
Matthew Einbinder <einbindermattwork@gmail.com>
2026-04-08 18:53:44 -0400

    Made script to auto make the deployment db

---
+++ scripts/db_deployment.sh
@@ -0,0 +1,5 @@
+mysql -e "create database if not exists bundles;"
+mysql 'bundles' -e "create user if not exists 'db_user'@'localhost' identified by 'db_pass';"
+mysql 'it490' -e "grant all on branches.* to 'db_user'@'localhost';"
+mysql 'it490' < schema_deployment.sql
+
