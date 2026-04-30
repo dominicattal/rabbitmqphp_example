4408dba34ed78feee48d2823aec85672c80af6f9
ddb33 <ddb33@njit.edu>
2026-04-30 12:23:49 -0400

    fixed error in ssl.sh

--- deploy/ssl.sh
+++ deploy/ssl.sh
@@ -7,7 +7,6 @@ mkdir -p /etc/apache2/ssl
 sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/apache2/ssl/ssl.key \
     -out /etc/apache2/ssl/ssl.crt \
-    # CHANGE THESE IP ADDRESSES TO YOURS
     -subj "/CN=100.73.150.33" \
     -addext "subjectAltName = IP:100.73.150.33"
 chmod 644 /etc/apache2/ssl/ssl.key
