54b4cef084f5334724d305881769ab79dfdc4242
dominicattal <attalientidominic@gmail.com>
2026-05-03 21:00:24 -0400

    distributed logging setup exchange

--- deploy/broker_cluster.sh
+++ deploy/broker_cluster.sh
@@ -10,12 +10,14 @@ rabbitmqctl set_user_tags broker_admin administrator
 
 rabbitmqctl  --vhost=cluster_vhost set_permissions broker_admin ".*" ".*" ".*"
 
-rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare exchange name="cluster_exchange" type="direct"
+rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare exchange name="cluster_exchange" type="topic"
 rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare queue name="web_queue"
 rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare queue name="db_queue"
 rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare queue name="db_listen_queue"
 rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare queue name="data_listen_queue"
+rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare queue name="log_queue"
 rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare binding source="cluster_exchange" destination="web_queue" routing_key="web"
 rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare binding source="cluster_exchange" destination="db_queue" routing_key="db"
 rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare binding source="cluster_exchange" destination="db_listen_queue" routing_key="db_listen"
 rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare binding source="cluster_exchange" destination="data_listen_queue" routing_key="data_listen"
+rabbitmqadmin --username=broker_admin --password=broker_admin --vhost=cluster_vhost declare binding source="cluster_exchange" destination="log_queue" routing_key="#"
